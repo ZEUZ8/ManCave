28e0885e0da4dfda8116f39c0b6b0adf50965695
sinan <ptsinan8590@gmail.com>
2024-04-26 15:20:47 +0530

    updated the main Home section and finished the cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import Image from "next/image";
 
 import { IoIosTimer } from "react-icons/io";
+import { FaPhone } from "react-icons/fa6";
+import { IoMdPerson } from "react-icons/io";
 
 export default function Home() {
   const admin = {
@@ -26,27 +28,26 @@ export default function Home() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#feda6a"
-            fill-opacity="1"
             d="M0,256L34.3,224C68.6,192,137,128,206,128C274.3,128,343,192,411,208C480,224,549,192,617,202.7C685.7,213,754,267,823,272C891.4,277,960,235,1029,197.3C1097.1,160,1166,128,1234,128C1302.9,128,1371,160,1406,176L1440,192L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"
           ></path>
         </svg>
       </div>
       <div className=" h-[80svh] flex  justify-center items-center">
-        <img src="barber.jpg" className="opacity-80" alt="" />
+        <img src="barber.jpg" className="opacity-70" alt="" />
         {/* <p>adisfnasdf </p> */}
       </div>
-      <div className="h-[80svh] p-[1.5rem]  absolute inset-0">
-        <div className="flex justify-center text-5xl font-bold  text-white">
+      <div className="h-[80svh] p-[1rem]  absolute inset-0">
+        <div className="flex justify-center text-4xl font-extrabold  text-white">
           Man Cave
         </div>
         <div className="flex flex-col gap-5 justify-center items-center h-full">
-          <div className="bg-[#feda6a]  shadow-lg py-5 px-2 w-full h-[30%] rounded-md grid ">
+          <div className="bg-[#feda6a]  shadow-lg py-5 px-2 w-full h-[10rem] rounded-md grid ">
             <div className="grid grid-cols-6 ">
               <div className="col-span-2 grid justify-center items-center">
                 <img
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQF5gWz6wsBbvNl28YWltxQlBuAHnnblGgt0ajNME_Ds9roH7fCEOQAUquyRNmfaJZSI&usqp=CAU"
                   alt="profile"
-                  className="h-[7rem] w-[10rem] rounded-md"
+                  className="h-[7rem] w-[10rem] rounded-full shadow-xl "
                 />
                 {/* <div>
                   <p className="font-bold ">BASIL</p>
@@ -54,29 +55,36 @@ export default function Home() {
               </div>
               <div className="col-span-4  px-3 flex flex-col items-center justify-between">
                 <div>
-                  <p className=" text-center font-bold text-xl pb-2">Basil</p>
-                  <p className="">contact : 8590183715</p>
-                  <p className="">Booking : 5</p>
+                  <p className=" text-center font-bold text-lg ">Basil</p>
+                  <div className="py-2 ">
+                    {/* <p className=""><FaPhone/> : </p> */}
+                    <div className="grid grid-cols-2 ">
+                      <p className="flex justify-start items-center gap-2"><span>Contact</span><FaPhone/></p>
+                      <p className="flex justify-start items-center gap-2"><span>:</span>8590183715</p>
+                    </div>
+                    <div className="grid grid-cols-2 ">
+                      <p className="flex justify-start items-center gap-2"><span>person</span><IoMdPerson/></p>
+                      <p className="flex justify-start items-center gap-2"><span>:</span>5</p>
+                    </div>
+                  </div>
                 </div>
                 <div className=" flex justify-center items-center w-full bg-white rounded-lg shadow-lg gap-1 active:scale-95">
-                  <p>
+                  <p className="font-bold text-lg">
                     <IoIosTimer />
                   </p>{" "}
-                  <p>Book Now</p>
+                  <p className="font-bold">Book Now</p>
                 </div>
               </div>
             </div>
           </div>
 
-      
-
-          <div className="bg-[#fea36a]  shadow-lg py-5 px-2 w-full h-[30%] rounded-md grid ">
+          <div className="bg-[#fea36a]  shadow-lg py-5 px-2 w-full h-[10rem] rounded-md grid ">
             <div className="grid grid-cols-6 ">
               <div className="col-span-2 grid justify-center items-center">
                 <img
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQF5gWz6wsBbvNl28YWltxQlBuAHnnblGgt0ajNME_Ds9roH7fCEOQAUquyRNmfaJZSI&usqp=CAU"
                   alt="profile"
-                  className="h-[7rem] w-[10rem] rounded-md"
+                  className="h-[7rem] w-[10rem] rounded-full shadow-xl "
                 />
                 {/* <div>
                   <p className="font-bold ">BASIL</p>
@@ -84,15 +92,24 @@ export default function Home() {
               </div>
               <div className="col-span-4  px-3 flex flex-col items-center justify-between">
                 <div>
-                  <p className=" text-center font-bold text-xl pb-2">Unais</p>
-                  <p className="">contact : 8590183715</p>
-                  <p className="">Booking : 5</p>
+                  <p className=" text-center font-bold text-lg ">Unais</p>
+                  <div className="py-2 ">
+                    {/* <p className=""><FaPhone/> : </p> */}
+                    <div className="grid grid-cols-2 ">
+                      <p className="flex justify-start items-center gap-2 font-bold"><span>Contact</span><FaPhone/></p>
+                      <p className="flex justify-start items-center gap-2"><span>:</span>8590183715</p>
+                    </div>
+                    <div className="grid grid-cols-2 ">
+                      <p className="flex justify-start items-center gap-2 font-bold"><span>person</span><IoMdPerson/></p>
+                      <p className="flex justify-start items-center gap-2"><span>:</span>5</p>
+                    </div>
+                  </div>
                 </div>
-                <div className=" flex justify-center items-center w-full bg-white rounded-lg shadow-lg gap-2 active:scale-95">
-                  <p>
+                <div className=" flex justify-center items-center w-full bg-white rounded-lg shadow-lg gap-1 active:scale-95 ">
+                  <p className="font-bold text-lg">
                     <IoIosTimer />
                   </p>{" "}
-                  <p>Book Now</p>
+                  <p className="font-bold">Book Now</p>
                 </div>
               </div>
             </div>
