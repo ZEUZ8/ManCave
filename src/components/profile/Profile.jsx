3e5b8ead8147f45dee3cdc26671deea29742579b
sinan <ptsinan8590@gmail.com>
2024-04-27 18:36:17 +0530

    updated the UI and rearranged the booking Button

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -20,19 +20,29 @@ const Profile = ({ name, designation }) => {
           <div className="flex flex-col items-center ">
             <h1 className="text-xl font-bold pb-3 pt-2">Bookings</h1>
             <div className="text-gray-500 py-2">
-                <div className="grid grid-cols-2 gap-1">
-                    <p>Booking</p><p>: 5</p>
-                </div>
-                <div className="grid grid-cols-2 gap-1">
-                    <p>Hour</p><p>: 2</p>
-                </div>
-                <div className="grid grid-cols-2 gap-1">
-                    <p>Status</p><p>: working</p>
-                </div>
+              <div className="grid grid-cols-2 gap-1">
+                <p>Booking</p>
+                <p>: 5</p>
+              </div>
+              <div className="grid grid-cols-2 gap-1">
+                <p>Hour</p>
+                <p>: 2</p>
+              </div>
+              <div className="grid grid-cols-2 gap-1">
+                <p>Status</p>
+                <p>: working</p>
+              </div>
+            </div>
+            <div class="w-full">
+              <div className="flex justify-center">
+                <a className="w-full flex justify-center items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Book
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div class="mt-4 md:mt-6">
+        {/* <div class="mt-4 md:mt-6">
           <p className="text-xs font-extralight text-gray-500 py-1">
             Book a slot next in row
           </p>
@@ -41,7 +51,7 @@ const Profile = ({ name, designation }) => {
               Book
             </a>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
