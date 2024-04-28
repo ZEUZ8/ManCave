b37b16de292c6f98a439e39d670a8de972aab62d
sinan <ptsinan8590@gmail.com>
2024-04-28 10:49:51 +0530

    resolved the code snipeet causing the zoominIn in the page

--- src/components/BookingModal/BookingModal.jsx
+++ src/components/BookingModal/BookingModal.jsx
@@ -91,7 +91,7 @@ const BookingModal = ({ handleModal }) => {
                     onBlur={handleBlur}
                     type="text"
                     name="userName"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-3  w-full"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg p-3 w-full"
                     placeholder="Name"
                   />
                   {errors.userName && touched.userName && (
