83a02ab68d028c623caf445849a045e4930e8ddb
sinan <ptsinan8590@gmail.com>
2024-04-28 10:41:33 +0530

    testing

--- src/components/BookingModal/BookingModal.jsx
+++ src/components/BookingModal/BookingModal.jsx
@@ -101,7 +101,7 @@ const BookingModal = ({ handleModal }) => {
                   )}
                 </div>
                 <div>
-                  <label class="block mb-2 text-sm font-medium text-gray-900 rounded-lg p-3 w-full">
+                  <label class="block mb-2 text-sm font-medium text-gray-900 p-3 w-full">
                     Mobile
                   </label>
                   <input
@@ -110,7 +110,7 @@ const BookingModal = ({ handleModal }) => {
                     value={values.mobile}
                     onBlur={handleBlur}
                     onChange={handleChange}
-                    className="bg-gray-50 border border-gray-300 text-gray-900   "
+                    className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg  "
                   />
                   {errors.mobile && touched.mobile && (
                     <h1 className="text-xs pt-2 text-rose-500 animate-bounce px-2">
