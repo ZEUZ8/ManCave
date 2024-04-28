343250c10cb2360967e8e8b9a53a40382b86bba5
sinan <ptsinan8590@gmail.com>
2024-04-28 08:42:00 +0530

    updated the meta tag for preventing the zoom in when clicking the input tag in the mobile size

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, user-scalable=no" />
       <body className={inter.className}>{children}</body>
     </html>
   );
