ca5f9c19dca598fb88fbe8a7d3f0c3ab4540fdb1
sinan <ptsinan8590@gmail.com>
2024-04-28 10:27:29 +0530

    testing

--- src/components/BookingModal/BookingModal.jsx
+++ src/components/BookingModal/BookingModal.jsx
@@ -91,7 +91,7 @@ const BookingModal = ({ handleModal }) => {
                     onBlur={handleBlur}
                     type="text"
                     name="userName"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3    "
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full"
                     placeholder="Name"
                   />
                   {errors.userName && touched.userName && (
