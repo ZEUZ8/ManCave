a13b763337ff29e3b69ef969073587a7be4af3c1
sinan <ptsinan8590@gmail.com>
2024-05-11 23:51:31 +0530

    getting started the admin section, started the admin booksing getting functionality

---
+++ src/app/admin/bookings/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      the admin page in teh console
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/api/admin/booking/Getting/route.js
@@ -0,0 +1,44 @@
+import { Connect } from "@/dbConfig/connect";
+import Bookings from "@/models/userBooking";
+import axios from "axios";
+import { NextResponse,NextRequest } from "next/server";
+
+Connect()
+
+export async function GET(request = NextRequest){
+    const currentDate = new Date();
+    const startOfCurrentDay = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate(),
+      0,
+      0,
+      0
+    );
+    const endOfCurrentDay = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate(),
+      23,
+      59,
+      59
+    );
+    try{
+        const response = await Bookings.find({$or:[{ createdAt: { $gte: startOfCurrentDay, $lte: endOfCurrentDay } },{ updatedAt: { $gte: startOfCurrentDay, $lte: endOfCurrentDay } }]})
+        if(response){
+            return NextResponse.json({
+                message: "Getting Bookings successfull",
+                success: true,
+                response,
+              });
+        }else{
+            return NextResponse.json({
+                message: "not bookings",
+                success:false,
+              });
+        }
+    }catch(err){
+        console.log(err,' consoling bookings getting error in the service page')
+        return NextResponse.json({error:err},{status:500})
+    }
+}
