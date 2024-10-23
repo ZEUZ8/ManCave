7bcb0d58fc1de5fa2b889e680a9f0e90644cc49e
sinan <ptsinan8590@gmail.com>
2024-10-24 00:27:37 +0530

    updated the mongodb Connection into backend and succesffully added the connection into the databas

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { Connect } = require("./src/config/databse");
+
 
 dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
 
-app.listen(port, () => console.log("server running in port ", port));
+
+
+function server() {
+  app.listen(port, () => console.log("server running in port ", port));
+}
+
+Connect(server)
